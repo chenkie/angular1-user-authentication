e662cbdb25cfb6cf768cecb65d49d73d2d9755f5
Ryan Chenkie <chenkie@users.noreply.github.com>
2017-08-15 18:04:19 -0400

    Update config.js

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,6 +1,6 @@
 export const ENV = 'DEV';
 export const DEBUG = (ENV !== 'PRODUCTION');
-export const API_BASE_URL = 'user-authentication-api-ocokqryugz.now.sh';
+export const API_BASE_URL = 'user-authentication-api.now.sh';
 export const API_URL = `https://${API_BASE_URL}/api`;
 
 export default { ENV, DEBUG, API_URL };
